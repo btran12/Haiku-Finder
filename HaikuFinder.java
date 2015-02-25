f055abc53f9039c8255c4f717f5c39972fabb7a1
Bao Tran <quybao95@Baos-MacBook-Pro.local>
2015-02-24 22:37:29 -0500

    Layout of program's functionalities

---
+++ HaikuFinder.java
@@ -0,0 +1,15 @@
+
+
+public class HaikuFinder {
+    public static void main(String[] args){
+        
+    }
+    
+    public void readIn(String fileLocation){
+        //Read in each word from the text file's location
+    }
+    
+    public void countSyllable(String word){
+        //Look for patterns within words
+    }
+}
